e6a4740007554a6b73e79722782e9d6cf0d50800
Anton Zhukov <janton20001@gmail.com>
2026-02-05 17:41:31 +0200

    Initial commit

---
+++ Source/GLab/GLabGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GLabGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLAB_API AGLabGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
